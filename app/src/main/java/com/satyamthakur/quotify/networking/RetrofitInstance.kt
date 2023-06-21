3457e18a6628e618206a91b21c38a2f5ddd42e1d
Satyam Thakur <sat.thakur20@gmail.com>
2023-06-21 19:25:11 +0530

    Creating RetrofitInstance object class

--- app/src/main/java/com/satyamthakur/quotify/networking/RetrofitInstance.kt
+++ app/src/main/java/com/satyamthakur/quotify/networking/RetrofitInstance.kt
@@ -1,7 +1,16 @@
 package com.satyamthakur.quotify.networking
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object RetrofitInstance {
 
-
+    val api: QuoteApi by lazy {
+        Retrofit.Builder()
+            .baseUrl("https://api.quotable.io/quotes/random")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(QuoteApi::class.java)
+    }
 
 }
